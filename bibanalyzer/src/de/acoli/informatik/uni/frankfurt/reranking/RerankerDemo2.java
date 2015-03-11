8f45f60b08372cb5c042d1c6cb14a3fd3278fc1f
niko <niko@niko-VPCEB4M1E>
2015-03-11 17:01:41 +0100

    - Improved html to crf conversion.

--- bibanalyzer/src/de/acoli/informatik/uni/frankfurt/reranking/RerankerDemo2.java
+++ bibanalyzer/src/de/acoli/informatik/uni/frankfurt/reranking/RerankerDemo2.java
@@ -1,13 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * *****************************************************************************
+ * Copyright (c) 2014 
+ * Christian Chiarcos, Niko Schenk 
+ * Applied Computational Linguistics Lab (ACoLi)
+ * Goethe-Universität Frankfurt am Main 
+ * http://acoli.cs.uni-frankfurt.de/en.html
+ * Robert-Mayer-Straße 10
+ * 60325 Frankfurt am Main
+ * 
+ * All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Niko Schenk - initial API and
+ * implementation.
+ * *****************************************************************************
  */
+
 
 package de.acoli.informatik.uni.frankfurt.reranking;
 
 /**
  *
+ * Idea: Use Reference Manager tokenization
+ * as input to BibAnalyzer pipeline.
+ * 
+ * -> Combine both analyses into one.
+ * 
  * @author niko
  */
 public class RerankerDemo2 {
